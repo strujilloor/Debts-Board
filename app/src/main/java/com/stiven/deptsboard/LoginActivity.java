a8f377f497a67c00ded347e70b0901a31d40a912
Stiven Trujillo <strujillo1293@gmail.com>
2018-06-01 13:05:46 -0500

    implementado el toolbar

--- app/src/main/java/com/stiven/deptsboard/LoginActivity.java
+++ app/src/main/java/com/stiven/deptsboard/LoginActivity.java
@@ -2,6 +2,7 @@ package com.stiven.deptsboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        showToolbar("Depts Board Login", false);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
