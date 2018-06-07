49ce7f6ed093b7a00ec3a92cd652204f45665848
Stiven Trujillo <strujillo1293@gmail.com>
2018-06-06 20:41:16 -0500

    creado y maqueteado la actividad DebtEditorActivity

---
+++ app/src/main/java/com/stiven/deptsboard/view/DebtEditorActivity.java
@@ -0,0 +1,15 @@
+package com.stiven.deptsboard.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stiven.deptsboard.R;
+
+public class DebtEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debt_editor);
+    }
+}
